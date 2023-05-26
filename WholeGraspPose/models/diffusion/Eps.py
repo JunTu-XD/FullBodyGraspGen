b5786af426b1146f0068e0f08922faa7d387e101
Jun <juntunju2012@gmail.com>
2023-05-26 23:44:50 +0200

    try 16

--- WholeGraspPose/models/diffusion/Eps.py
+++ WholeGraspPose/models/diffusion/Eps.py
@@ -12,10 +12,9 @@ class Eps(nn.Module):
         self.time_emb_dim = D
         # self.model = UNet1D(drop_out_p=0)
         # self.model = FlatPush(depth=5,drop_out_p=0)
-        self.model = TransformerDenoising(seq_len=8, vec_dim=D, drop_out_p=0.3, heads=8, depth=4)
+        self.model = TransformerDenoising(seq_len=8, vec_dim=D+512, drop_out_p=0.3, heads=8, depth=4)
 
     def forward(self, x, t, condition):
         t_emb = get_timestep_embedding(timesteps=t, embedding_dim=self.time_emb_dim)
-        _x = torch.cat(((x + t_emb), condition), dim=1)
-
+   
         return self.model(feature_vec=x, time=t_emb, condition=condition)
