7ded78c8268dfa6f7dbd59b944e97d28ea7b99fd
Jun <juntunju2012@gmail.com>
2023-06-13 14:32:02 +0200

    train

--- train_diffusion.py
+++ train_diffusion.py
@@ -19,6 +19,8 @@ class DiffusionTrainer:
                                                     batch_size=cfg.batch_size)
 
         self.model = Eps(D=cfg.x_dim)
+        if self.cfg.trained_diffusion is not None:
+            self.load_model()
         self.diffusion = DDPM()
         self.diffusion.model = self.model
 
@@ -46,22 +48,49 @@ class DiffusionTrainer:
                 self.writer.flush()
 
                 self.optimizer.step()
-    def val(self):
-        sample_batch = 512
-        cond_1 = torch.nn.functional.one_hot(torch.ones((sample_batch, )).long() * 10, 23).float()
-        samples_1 = self.diffusion.sample(batch_size=sample_batch, ddim=False, condition= cond_1)
 
-        cond_0 = torch.nn.functional.one_hot(torch.ones((sample_batch, )).long() * 0, 23).float()
-        samples_0 = self.diffusion.sample(batch_size=sample_batch, ddim=False, condition=cond_0)
-        print(torch.norm(torch.mean(samples_0, dim =0) - torch.mean(samples_1, dim=0)))
+    def load_model(self):
+        self.model.load_state_dict(torch.load(self.cfg.trained_diffusion, map_location=self.device), strict=False)
+
+    def dist_metrics(self, p=2):
+        sample_batch = 512
+
+        samples = []
+        mean_samples = []
+        for label in range(2):
+            cond_ = torch.nn.functional.one_hot(torch.ones((sample_batch, )).long() * label, 23).float()
+            samples_ = self.diffusion.sample(batch_size=sample_batch, ddim=False, condition= cond_)
+            samples.append(samples_)
+            mean_samples.append(samples_.mean(dim=0)[None, :])
+
+        internal_dist = []
+        for samples_l in samples:
+            dist = torch.cdist(samples_l, samples_l, p = p)
+            mean_internal_dist = torch.sum(dist) / (sample_batch**2 - samples)
+            internal_dist.append(mean_internal_dist)
+        mean_samples = torch.cat(mean_samples)
+        external_dist = torch.cdist(mean_samples)
         return
 
     def save(self):
         torch.save(self.model.state_dict(), f"{self.cfg.save_folder}/diffusion_model.pt")
 
-    def train_imporved_ddpm(self):
-        pass
+    def original_data(self):
+        data_dict = torch.load("dataset/saga_male_latent_label.pt", map_location=self.device)
+        label_inds = torch.argmax(data_dict['label'], dim=1)
+        labels_mu = {i:[] for i in range(22)}
+        labels_var = {i:[] for i in range(22)}
 
+        mius = [[] for i in range(22)]
+        for i in range(data_dict['mu'].shape[0]):
+            mu = data_dict['mu'][i]
+            var = data_dict['var'][i]
+            labels_mu[int(label_inds[i])].append(mu)
+            labels_var[int(label_inds[i])].append(var)
+        mius_tensor = []
+        for i, m in labels_mu.items():
+            mius.append(torch.cat(m).mean(dim=0))
+        return
 if __name__=="__main__":
     exp_name = "test"
 
@@ -73,11 +102,12 @@ if __name__=="__main__":
         "trained_model":None,
         "epoch":2,
         "save_folder":f"logs/{exp_name}/",
-        "lr":1e-4
+        "lr":1e-4,
+        "trained_diffusion": f"logs/{exp_name}/diffusion_model.pt",
     })
 
     trainer = DiffusionTrainer(cfg)
-    trainer.train_ddpm()
-    trainer.val()
-    trainer.save()
-
+    # trainer.train_ddpm()
+    trainer.dist_metrics()
+    # trainer.save()
+    trainer.original_data()
